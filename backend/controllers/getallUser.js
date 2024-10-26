ada2e30dc61933db080067aef6ab1e7697556253
himanshumittal168 <himanshumittal168@gmail.com>
2024-10-26 17:13:20 +0530

    backend_complete

---
+++ backend/controllers/getallUser.js
@@ -0,0 +1,21 @@
+// WRITE AN API to get all the Users
+
+const User=require("../model/User");
+
+const getAllUsers = async (req, res) => 
+{
+    try 
+    {
+      const users = await User.find({});
+      res.status(200).json({ message: "User fetcch successfull", users });
+    }
+    catch (error) 
+    {
+      console.error("Error fetching users", error);
+      res.status(500).json({ message: "Error fetching users" });
+    }
+};
+
+
+module.exports=getAllUsers;
+
---
+++ readme.txt
@@ -0,0 +1,15 @@
+Schema->
+User
+    Name ->string
+    Email->string
+    Title->string
+    Department->string
+    Role->string
+    Image->?  (API) ->string  (url)
+
+
+APIs
+    post -> createUser
+    get  ->getalldata
+
+
